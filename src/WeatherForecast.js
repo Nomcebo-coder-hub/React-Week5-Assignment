06560ab38a2361dac68c46c0c1074f60395cd30c
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-05-08 00:35:26 +0200

    Added forecast for thursday

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,16 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast() {
-  return;
-  <div className="WeatherForecast">
-    <div className="row">
-      <div className="col"></div>
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div>Thur</div>
+          <WeatherIcon code="01d" />
+          19 10
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
