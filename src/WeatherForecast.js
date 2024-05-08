416db8655fee63f42580a882571d075fbd87d821
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-05-08 14:45:23 +0200

    Added useState to React and added if else statements

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,24 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
   }
 
   if (loaded) {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-
-    axios.get(apiUrl).then(handleResponse);
-
-    return null;
-  } else {
     return (
       <div className="WeatherForecast">
         <div className="row">
@@ -33,5 +26,14 @@ export default function WeatherForecast(props) {
         </div>
       </div>
     );
+  } else {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
   }
 }
