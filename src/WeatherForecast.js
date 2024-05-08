d0faf58376e7de315de63366c89ca3196861d25b
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-05-08 14:32:11 +0200

    Update Weather.js and WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,26 +1,37 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let longitude = 40.7;
-  let latitude = 74;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thur</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span>
-            <span className="WeatherForecast-temperature-min">10°</span>
+  if (loaded) {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  } else {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Thur</div>
+            <WeatherIcon code="01d" size={36} />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-max">19°</span>
+              <span className="WeatherForecast-temperature-min">10°</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
